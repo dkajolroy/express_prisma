1fe647a52148074f02828ac34d3e309b15d17082
kajolroy <shrikajol@gmail.com>
2023-01-08 12:12:07 +0600

    body parser added

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 //config
 app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 dotenv.config();
 
 //Router Setup
